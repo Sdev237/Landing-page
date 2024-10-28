1cb3d28aa71feb3ad87d21e9abae3eb49158eacc
Sdev237 <sdevsap@gmail.com>
2024-10-28 13:04:33 -0400

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,48 @@ import { Container } from './Container'
 import { social } from '../constants'
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear()
+  
+  const renderSocialLinks = () => (
+    <div className='flex items-center gap-4'>
+      {social.map(({id, alt, link, ico}) => (
+        <a 
+          key={id}
+          title={alt} 
+          href={link}
+          target="_blank" 
+          rel="noreferrer"
+        >
+          <img src={ico} alt={alt} width={20} height={20} />
+        </a>
+      ))}
+    </div>
+  )
+
+  const renderLinks = () => (
+    <div className='space-x-4'>
+      {['Terms of Service', 'Privacy Policy', 'About'].map(text => (
+        <a key={text} href='#/' target="_blank" rel="noreferrer">
+          {text}
+        </a>
+      ))}
+    </div>
+  )
+
   return (
     <div className='bg-n-7 py-5 lg:py-12 mt-5 lg:mt-16'>
-    <Container>
-      <div className="flex flex-col gap-3 lg:flex-row lg:gap-1 items-center justify-between body-3 text-n-3">
-        <div className='flex items-center gap-4'>
-          {social.map((link) => (
-            <a key={link.id} title={link.alt} href={link.link} target="_blank" rel="noreferrer">
-              <img src={link.ico} alt={link.alt} width={20} height={20} />
+      <Container className="footer">
+        <div className="flex flex-col gap-3 lg:flex-row lg:gap-1 items-center justify-between body-3 text-n-3">
+          {renderSocialLinks()}
+          {renderLinks()}
+          <div>
+            &copy; {currentYear} All right reserved{" "}
+            <a href='#/' target="_blank" rel="noreferrer">
+              Sdev Sap & Remote Monkey
             </a>
-          ))}
+          </div>
         </div>
-        <div className='space-x-4'>
-          <a href='#/' target="_blank" rel="noreferrer">Terms of Service</a>
-          <a href='#/' target="_blank" rel="noreferrer">Privacy Policy</a>
-          <a href='#/' target="_blank" rel="noreferrer">About</a>
-        </div>
-        <div>&copy; 2024 All right reserved{" "}
-          <a href='#/' target="_blank" rel="noreferrer">Sdev Sap & Remote Monkey</a>
-        </div>
-      </div>
-    </Container>
-  </div>
+      </Container>
+    </div>
   )
 }
